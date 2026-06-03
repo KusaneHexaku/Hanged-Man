26752c1847fb5fea1ec7030c6245239bb83ac65d
KusaneHexaku <52283992+KusaneHexaku@users.noreply.github.com>
2026-06-04 02:28:33 +0700

    Jank workaround fix for Hands Down to make hands stay at 0

--- items/Challenges/handsDownChallenge.lua
+++ items/Challenges/handsDownChallenge.lua
@@ -27,6 +27,25 @@ SMODS.Challenge {
 
         if context.main_scoring then
             G.GAME.hands_down_hand_remaining = G.GAME.hands_down_hand_remaining - 1
+            
+        end
+
+        if context.starting_shop and context.main_eval and G.GAME.hands_down_hand_remaining < 1 then
+            G.GAME.round_resets.hands = G.GAME.hands_down_hand_remaining
+            ease_hands_played(-1)
+        end
+
+        if context.setting_blind and G.GAME.hands_down_hand_remaining < 1 then
+            G.GAME.round_resets.hands = G.GAME.hands_down_hand_remaining
+            ease_hands_played(-1)
+            -- Credits to Winter <3
+            G.STATE = G.STATES.GAME_OVER
+            if not G.GAME.won and not G.GAME.seeded and not G.GAME.challenge then
+                G.PROFILES[G.SETTINGS.profile].high_scores.current_streak.amt = 0
+            end
+            G:save_settings()
+            G.FILE_HANDLER.force = true
+            G.STATE_COMPLETE = false
         end
 
     end
